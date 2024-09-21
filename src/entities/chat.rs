88537b6679fa72ee5ef395e477e4c3db2cb0c17e
ENCRYPTED_FOREVER <vip.famy@mail.ru>
2024-09-21 23:19:32 +0300

    Use imports instead of fully-qualified names in chat helper methods

--- src/entities/chat.rs
+++ src/entities/chat.rs
@@ -62,6 +62,34 @@ use crate::api::API;
 use crate::entities::chat_permissions::ChatPermissions;
 use crate::impl_trait;
 
+use crate::methods::approve_chat_join_request::ApproveChatJoinRequestRequest;
+use crate::methods::ban_chat_member::BanChatMemberRequest;
+use crate::methods::ban_chat_sender_chat::BanChatSenderChatRequest;
+use crate::methods::create_chat_invite_link::CreateChatInviteLinkRequest;
+use crate::methods::decline_chat_join_request::DeclineChatJoinRequestRequest;
+use crate::methods::delete_chat_photo::DeleteChatPhotoRequest;
+use crate::methods::delete_chat_sticker_set::DeleteChatStickerSetRequest;
+use crate::methods::edit_chat_invite_link::EditChatInviteLinkRequest;
+use crate::methods::export_chat_invite_link::ExportChatInviteLinkRequest;
+use crate::methods::get_chat::GetChatRequest;
+use crate::methods::get_chat_administrators::GetChatAdministratorsRequest;
+use crate::methods::get_chat_member::GetChatMemberRequest;
+use crate::methods::get_chat_member_count::GetChatMemberCountRequest;
+use crate::methods::leave_chat::LeaveChatRequest;
+use crate::methods::promote_chat_member::PromoteChatMemberRequest;
+use crate::methods::restrict_chat_member::RestrictChatMemberRequest;
+use crate::methods::revoke_chat_invite_link::RevokeChatInviteLinkRequest;
+use crate::methods::send_chat_action::SendChatActionRequest;
+use crate::methods::set_chat_administrator_custom_title::SetChatAdministratorCustomTitleRequest;
+use crate::methods::set_chat_description::SetChatDescriptionRequest;
+use crate::methods::set_chat_permissions::SetChatPermissionsRequest;
+use crate::methods::set_chat_photo::SetChatPhotoRequest;
+use crate::methods::set_chat_sticker_set::SetChatStickerSetRequest;
+use crate::methods::set_chat_title::SetChatTitleRequest;
+use crate::methods::unban_chat_member::UnbanChatMemberRequest;
+use crate::methods::unban_chat_sender_chat::UnbanChatSenderChatRequest;
+use crate::methods::unpin_all_chat_messages::UnpinAllChatMessagesRequest;
+
 impl Chat {
     // Returns Chat's title for groups and User::full_name for private chats
     pub fn full_name(&self) -> String {
@@ -93,44 +121,27 @@ pub trait TgChat {
         }
     }
 
-    fn unpin_all_messages<'a>(
-        &'a self,
-        api: &'a API,
-    ) -> crate::methods::unpin_all_chat_messages::UnpinAllChatMessagesRequest {
+    fn unpin_all_messages<'a>(&'a self, api: &'a API) -> UnpinAllChatMessagesRequest<'a> {
         api.unpin_all_chat_messages(self.id())
     }
 
-    fn get_member_count<'a>(
-        &'a self,
-        api: &'a API,
-    ) -> crate::methods::get_chat_member_count::GetChatMemberCountRequest {
+    fn get_member_count<'a>(&'a self, api: &'a API) -> GetChatMemberCountRequest<'a> {
         api.get_chat_member_count(self.id())
     }
 
-    fn get_administrators<'a>(
-        &'a self,
-        api: &'a API,
-    ) -> crate::methods::get_chat_administrators::GetChatAdministratorsRequest {
+    fn get_administrators<'a>(&'a self, api: &'a API) -> GetChatAdministratorsRequest<'a> {
         api.get_chat_administrators(self.id())
     }
 
-    fn get_member<'a>(
-        &'a self,
-        api: &'a API,
-        user_id: impl Into<i64>,
-    ) -> crate::methods::get_chat_member::GetChatMemberRequest {
+    fn get_member<'a>(&'a self, api: &'a API, user_id: impl Into<i64>) -> GetChatMemberRequest<'a> {
         api.get_chat_member(self.id(), user_id)
     }
 
-    fn get_full<'a>(&'a self, api: &'a API) -> crate::methods::get_chat::GetChatRequest {
+    fn get_full<'a>(&'a self, api: &'a API) -> GetChatRequest<'a> {
         api.get_chat(self.id())
     }
 
-    fn ban_member<'a>(
-        &'a self,
-        api: &'a API,
-        user_id: impl Into<i64>,
-    ) -> crate::methods::ban_chat_member::BanChatMemberRequest {
+    fn ban_member<'a>(&'a self, api: &'a API, user_id: impl Into<i64>) -> BanChatMemberRequest<'a> {
         api.ban_chat_member(self.id(), user_id)
     }
 
@@ -138,7 +149,7 @@ pub trait TgChat {
         &'a self,
         api: &'a API,
         user_id: impl Into<i64>,
-    ) -> crate::methods::unban_chat_member::UnbanChatMemberRequest {
+    ) -> UnbanChatMemberRequest<'a> {
         api.unban_chat_member(self.id(), user_id)
     }
 
@@ -146,7 +157,7 @@ pub trait TgChat {
         &'a self,
         api: &'a API,
         sender_chat_id: impl Into<i64>,
-    ) -> crate::methods::ban_chat_sender_chat::BanChatSenderChatRequest {
+    ) -> BanChatSenderChatRequest<'a> {
         api.ban_chat_sender_chat(self.id(), sender_chat_id)
     }
 
@@ -154,7 +165,7 @@ pub trait TgChat {
         &'a self,
         api: &'a API,
         sender_chat_id: impl Into<i64>,
-    ) -> crate::methods::unban_chat_sender_chat::UnbanChatSenderChatRequest {
+    ) -> UnbanChatSenderChatRequest<'a> {
         api.unban_chat_sender_chat(self.id(), sender_chat_id)
     }
 
@@ -163,16 +174,11 @@ pub trait TgChat {
         api: &'a API,
         user_id: impl Into<i64>,
         custom_title: impl Into<String>,
-    ) -> crate::methods::set_chat_administrator_custom_title::SetChatAdministratorCustomTitleRequest
-    {
+    ) -> SetChatAdministratorCustomTitleRequest<'a> {
         api.set_chat_administrator_custom_title(self.id(), user_id, custom_title)
     }
 
-    fn set_title<'a>(
-        &'a self,
-        api: &'a API,
-        title: impl Into<String>,
-    ) -> crate::methods::set_chat_title::SetChatTitleRequest {
+    fn set_title<'a>(&'a self, api: &'a API, title: impl Into<String>) -> SetChatTitleRequest<'a> {
         api.set_chat_title(self.id(), title)
     }
 
@@ -180,7 +186,7 @@ pub trait TgChat {
         &'a self,
         api: &'a API,
         description: Option<impl Into<String>>,
-    ) -> crate::methods::set_chat_description::SetChatDescriptionRequest {
+    ) -> SetChatDescriptionRequest<'a> {
         if let Some(description) = description {
             api.set_chat_description(self.id()).description(description)
         } else {
@@ -192,7 +198,7 @@ pub trait TgChat {
         &'a self,
         api: &'a API,
         permissions: impl Into<ChatPermissions>,
-    ) -> crate::methods::set_chat_permissions::SetChatPermissionsRequest {
+    ) -> SetChatPermissionsRequest<'a> {
         api.set_chat_permissions(self.id(), permissions)
     }
 
@@ -200,7 +206,7 @@ pub trait TgChat {
         &'a self,
         api: &'a API,
         photo: impl Into<InputFile>,
-    ) -> crate::methods::set_chat_photo::SetChatPhotoRequest {
+    ) -> SetChatPhotoRequest<'a> {
         api.set_chat_photo(self.id(), photo)
     }
 
@@ -208,7 +214,7 @@ pub trait TgChat {
         &'a self,
         api: &'a API,
         sticker_set_name: impl Into<String>,
-    ) -> crate::methods::set_chat_sticker_set::SetChatStickerSetRequest {
+    ) -> SetChatStickerSetRequest<'a> {
         api.set_chat_sticker_set(self.id(), sticker_set_name)
     }
 
@@ -216,7 +222,7 @@ pub trait TgChat {
         &'a self,
         api: &'a API,
         action: impl Into<crate::methods::send_chat_action::SendChatActionAction>,
-    ) -> crate::methods::send_chat_action::SendChatActionRequest {
+    ) -> SendChatActionRequest<'a> {
         api.send_chat_action(self.id(), action)
     }
 
@@ -224,32 +230,23 @@ pub trait TgChat {
         &'a self,
         api: &'a API,
         invite_link: impl Into<String>,
-    ) -> crate::methods::edit_chat_invite_link::EditChatInviteLinkRequest {
+    ) -> EditChatInviteLinkRequest<'a> {
         api.edit_chat_invite_link(self.id(), invite_link)
     }
 
-    fn leave<'a>(&'a self, api: &'a API) -> crate::methods::leave_chat::LeaveChatRequest {
+    fn leave<'a>(&'a self, api: &'a API) -> LeaveChatRequest<'a> {
         api.leave_chat(self.id())
     }
 
-    fn delete_photo<'a>(
-        &'a self,
-        api: &'a API,
-    ) -> crate::methods::delete_chat_photo::DeleteChatPhotoRequest {
+    fn delete_photo<'a>(&'a self, api: &'a API) -> DeleteChatPhotoRequest<'a> {
         api.delete_chat_photo(self.id())
     }
 
-    fn delete_sticker_set<'a>(
-        &'a self,
-        api: &'a API,
-    ) -> crate::methods::delete_chat_sticker_set::DeleteChatStickerSetRequest {
+    fn delete_sticker_set<'a>(&'a self, api: &'a API) -> DeleteChatStickerSetRequest<'a> {
         api.delete_chat_sticker_set(self.id())
     }
 
-    fn export_invite_link<'a>(
-        &'a self,
-        api: &'a API,
-    ) -> crate::methods::export_chat_invite_link::ExportChatInviteLinkRequest {
+    fn export_invite_link<'a>(&'a self, api: &'a API) -> ExportChatInviteLinkRequest<'a> {
         api.export_chat_invite_link(self.id())
     }
 
@@ -257,7 +254,7 @@ pub trait TgChat {
         &'a self,
         api: &'a API,
         invite_link: impl Into<String>,
-    ) -> crate::methods::revoke_chat_invite_link::RevokeChatInviteLinkRequest {
+    ) -> RevokeChatInviteLinkRequest<'a> {
         api.revoke_chat_invite_link(self.id(), invite_link)
     }
 
@@ -265,7 +262,7 @@ pub trait TgChat {
         &'a self,
         api: &'a API,
         user_id: impl Into<i64>,
-    ) -> crate::methods::approve_chat_join_request::ApproveChatJoinRequestRequest {
+    ) -> ApproveChatJoinRequestRequest<'a> {
         api.approve_chat_join_request(self.id(), user_id)
     }
 
@@ -273,14 +270,11 @@ pub trait TgChat {
         &'a self,
         api: &'a API,
         user_id: impl Into<i64>,
-    ) -> crate::methods::decline_chat_join_request::DeclineChatJoinRequestRequest {
+    ) -> DeclineChatJoinRequestRequest<'a> {
         api.decline_chat_join_request(self.id(), user_id)
     }
 
-    fn create_invite_link<'a>(
-        &'a self,
-        api: &'a API,
-    ) -> crate::methods::create_chat_invite_link::CreateChatInviteLinkRequest {
+    fn create_invite_link<'a>(&'a self, api: &'a API) -> CreateChatInviteLinkRequest<'a> {
         api.create_chat_invite_link(self.id())
     }
 
@@ -288,7 +282,7 @@ pub trait TgChat {
         &'a self,
         api: &'a API,
         user_id: impl Into<i64>,
-    ) -> crate::methods::promote_chat_member::PromoteChatMemberRequest {
+    ) -> PromoteChatMemberRequest<'a> {
         api.promote_chat_member(self.id(), user_id)
     }
 
@@ -297,7 +291,7 @@ pub trait TgChat {
         api: &'a API,
         user_id: impl Into<i64>,
         permissions: impl Into<ChatPermissions>,
-    ) -> crate::methods::restrict_chat_member::RestrictChatMemberRequest {
+    ) -> RestrictChatMemberRequest<'a> {
         api.restrict_chat_member(self.id(), user_id, permissions)
     }
 }
