8bb1280cf7848caecc26dc2770fd0b332fee0016
Encrypted <48645524+ENCRYPTEDFOREVER@users.noreply.github.com>
2026-06-08 13:56:04 +0300

    Fix wrongly determined 'codec' field type

--- src/entities/video_quality.rs
+++ src/entities/video_quality.rs
@@ -18,24 +18,11 @@ pub struct VideoQuality {
     pub height: i64,
 
     /// Codec that was used to encode the video, for example, “h264”, “h265”, or “av01”
-    pub codec: VideoQualityCodec,
+    pub codec: String,
 
     /// *Optional*. File size in bytes. It can be bigger than 2^31 and some programming languages may have difficulty/silent defects in interpreting it. But it has at most 52 significant bits, so a signed 64-bit integer or double-precision float type are safe for storing this value.
     #[serde(default, skip_serializing_if = "Option::is_none")]
     pub file_size: Option<i64>,
-}
-
-/// Codec that was used to encode the video, for example, “h264”, “h265”, or “av01”
-#[derive(Debug, Clone, Copy, Default, PartialEq, Eq, Serialize, Deserialize)]
-pub enum VideoQualityCodec {
-    /// `h265`
-    #[default]
-    #[serde(rename = "h265")]
-    H265,
-
-    /// `av01`
-    #[serde(rename = "av01")]
-    Av01,
 }
 
 // Divider: all content below this line will be preserved after code regen
